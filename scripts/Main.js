bc6708676ecea2ecf370d76d1b4ca77593f64abe
Gassayping <Dev_Gassayping@proton.me>
2023-01-03 13:39:55 -0800

    The actual functionality for TPS that wasnt in last commit 🤦‍♂️

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,6 +1,6 @@
 import "./ReloadScript";
 import {overworld} from "./Exports";
-import { world } from "@minecraft/server";
+import {world} from "@minecraft/server";
 
 let tickLengths = [];
 let tickTotals = 0;
@@ -10,10 +10,15 @@ world.events.tick.subscribe(e => tick(e))
 function tick(t) {
     tickLengths.unshift(t.deltaTime);
     tickTotals += t.deltaTime;
-    if (tickLengths.length >= 100) {
+    while (tickTotals > 1) {
+        tickTotals -= tickLengths[tickLengths.length-1];
         tickLengths.pop();
     }
-    try{tickTotals -= tickLengths[20];} catch{}
-    let tps = tickTotals/20;
-    overworld.runCommandAsync(`title @a actionbar TPS:${tps}/20`);
+    if (tickLengths.length-1 < 5) {
+        overworld.runCommandAsync(`title @a actionbar TPS: §c${tickLengths.length}/20`);
+    } else if(tickLengths.length-1 < 15) {
+        overworld.runCommandAsync(`title @a actionbar TPS: §e${tickLengths.length}/20`);
+    } else {
+        overworld.runCommandAsync(`title @a actionbar TPS: §2${tickLengths.length}/20`);
+    }
 }
